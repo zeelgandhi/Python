432f6d4361b699b267b57c7ff3e9aff5e57de9ab
Zeel <zrg3@njit.edu>
2018-02-07 16:05:32 -0600

    Hi

--- hungry/__main__.py
+++ hungry/__main__.py
@@ -1,1 +1,10 @@
-print("Hello world!")
+import argparse
+
+
+parser = argparse.ArgumentParser(prog="hungry")
+parser.add_argument('--user_id',type = int)
+
+args = parser.parse_args()
+
+print(args)
+print(args.user_id)
