840b708dce5b61ebe3ef7dc72760e44d0cde1023
Zeel <zrg3@njit.edu>
2018-02-06 18:14:34 -0600

    Updated more

--- tempelates.py
+++ tempelates.py
@@ -8,10 +8,25 @@ open("\hi\this\is\a\file.txt").read()
 
 open("/abc/ad/file.txt").read()
 """
-#os.path.join()
+#os.path.join() cwd-current working directory
 def get_template_path(path):
-    file_path = os.path.join(os.getcwd(),path)  #cwd-current working directory 
+    file_path = os.path.join(os.getcwd(),path)  
+    if not os.path.isfile(file_path):
+        return Exception ("This is not a valid template path %s"% (file_path))
     return file_path
 
-file_ = 'this/is/a/file.txt'
-print(get_template_path(file_))
+def get_template(path):
+    file_path = get_template_path(path)
+    return open(file_path).read()
+
+def render_context(template_string,context):
+    return template_string.format(**context)
+
+file_ = 'tempelates/email_message.txt'
+template = get_template(file_)
+context = {
+    "name": "Zeel",
+    "date": None,
+    "total": None
+}
+print(render_context(template,context))
