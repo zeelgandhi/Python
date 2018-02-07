f4185b8e778ffca38022bc3874c4becb8ad7436d
Zeel <zrg3@njit.edu>
2018-02-07 12:57:21 -0600

    Updated on 7

---
+++ data.csv/csv.py
@@ -0,0 +1,10 @@
+import csv
+with open("date.csv", "w+") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["Title","Description"])
+    writer.writerow(["Row1", "Some descr."])
+    
+    
+exit()
+
+
