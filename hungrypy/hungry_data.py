6114f2c0a5f7c8f487a80c7529cbf43106ca5203
Zeel <zrg3@njit.edu>
2018-02-08 14:32:26 -0600

    updatesss

--- hungrypy/hungry_data.py
+++ hungrypy/hungry_data.py
@@ -1,19 +1,18 @@
 import csv
 
 def get_length(file_path):
-    return
+    return 1
    
 def append_data(file_path, name, email):
     fieldnames = ['id', 'name', 'email']
-    #numb of rows?
-    next_id = get_length(file_path)
+    #the num of rows?
+    next_id= get_length(file_path)
     with open(file_path, "a") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-        writer.writeheader()
+        #writer.writeheader()
         writer.writerow({
             "id": next_id,
             "name": name,
             "email": email
         })
-
-append_data("data.csv", "Zeel", "zeelphysics@gmail.com")
+append_data("data.csv", "Zeel", "zeelphysics@gmai")
