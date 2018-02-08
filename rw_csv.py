31953cb6024ad80f4e7f495116508bd2dca40bf5
Zeel <zrg3@njit.edu>
2018-02-08 12:32:35 -0600

    Hi

--- rw_csv.py
+++ rw_csv.py
@@ -5,11 +5,13 @@ with open("data.csv", "w+") as csvfile:
     writer.writerow(["Column1", "Column2"])
 
 
-with open("data.csv", "r") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        print(row)
+
 
 with open("data.csv", "a") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["data1", "data2"])
+
+with open("data.csv", "r") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        print(row)
