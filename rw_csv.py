50ce09e696c0d0c0b8711bd80fc5a6ca70529bde
Zeel <zrg3@njit.edu>
2018-02-08 12:51:39 -0600

    again

--- rw_csv.py
+++ rw_csv.py
@@ -2,16 +2,24 @@ import csv
 with open("data.csv", "w+") as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(["Column1", "Column2"])
-    writer.writerow(["Column1", "Column2"])
-
-
+    writer.writerow(["data1", "data2"])
 
 
 with open("data.csv", "a") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["data1", "data2"])
+    writer.writerow(["data3", "data4"])
 
 with open("data.csv", "r") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
         print(row)
+
+with open("data.csv", "r") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
+
+with open("data.csv", "a") as csvfile:
+    filenames = ["id", "title"]
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer.writerow([{"id": 123, "title": "new title"}]
