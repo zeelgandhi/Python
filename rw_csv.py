b706059b0ded6ebb3977d9aaf49541282b94255d
Zeel <zrg3@njit.edu>
2018-02-08 13:14:54 -0600

    new

--- rw_csv.py
+++ rw_csv.py
@@ -17,8 +17,11 @@ with open("data.csv", "r") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         print(row)
+    
 
-with open("data.csv", "a") as csvfile:
-    fieldnames = ["Column1", "Column2"]
+with open("data.csv", "w") as csvfile:
+    fieldnames = ["id", "title"]
     writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
-    writer.writerow({"Column1": 123, "Column2": "new title"})
+    writer.writeheader()
+    writer.writerow({"id": 123,"title": "new title"})
+   
